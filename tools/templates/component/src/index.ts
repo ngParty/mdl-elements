fc90cd56fb564ea322d0628c03b3d94a0a421615
Mario Vejlupek <mario@vejlupek.cz>
2016-10-18 15:18:50 +0100

    feat(plop): add basic component generator

---
+++ tools/templates/component/src/index.ts
@@ -0,0 +1,6 @@
+import { define } from 'skatejs';
+import { {{properCase name}} } from './{{properCase name}}';
+
+export { {{properCase name}} } from './{{properCase name}}';
+
+define( {{properCase name}}.is, {{properCase name}} );
